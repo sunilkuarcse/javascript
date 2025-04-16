668e647d0249df2422c5122c6e5a149cbebc9d36
sunilkuarcse <216sunildd@gmail.com>
2025-04-16 11:07:18 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+// javascript variables
+
+let num=12345;
+let Name="sunildabriya"
+const numm1=25422;
+let age=21;
+
+console.log(num);
+console.log(Name);
+console.log(numm1);
+console.log(age);
+
+let message;
+console.log(message);
+
+
+
+
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,40 @@
+// datatypes---------------------in javascrit--
+
+// js string-----
+const Name="sunil";
+const Name1="dabriya";
+
+// used let 
+let collageName="sobhasariya";
+console.log(Name);
+console.log(Name1)
+console.log(collageName);
+
+// number js----------
+let num1=123456;
+let x=10;
+let y=20;
+console.log(x , y);
+
+// boolean-------
+const iscodingfun=false;
+const ismathfun=true;
+
+console.log(iscodingfun , ismathfun);
+
+// object in js -----
+let object = {
+          name : "sunil",
+          rollNumber:24,
+          age : 21,
+          isSweet:true,
+
+}
+console.log(object);
+
+// array in javascript--------
+
+let fruits=["apple" , "banana" , "orange" , "grapes"]
+console.log(fruits[0]);
+console.log(typeof num1);
+
