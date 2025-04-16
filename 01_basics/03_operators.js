8b37bd7373b761dc299877e464d3a78fe86c4d72
sunilkuarcse <216sunildd@gmail.com>
2025-04-16 11:08:12 +0000

    datatypes

---
+++ 01_basics/03_operators.js
@@ -0,0 +1,55 @@
+// operators in javascript---------
+
+// let x=10;
+// let y=20;
+// console.log(x+y);
+
+// const x1=20;
+// const y1=35;
+// const sum=x1+y1;
+// console.log(sum);
+
+// let a=10;
+// let b=20;
+
+// a+=b;
+// console.log(a);
+
+// let firstName="sunil";
+// let lastName= "dabriya";
+
+
+// let fullName = firstName + lastName;
+// console.log(fullName);
+// let baby =" baby";
+// firstName +=baby;
+
+// console.log(firstName);
+
+
+// const text = "lorem epsom";
+// const text1= "dolor sit amet";
+// const text2 = "consectetur adipiscing elit"
+
+// const sentance  = text + text1 + text2;
+// console.log(sentance);
+
+// comparoson operator in js-------
+
+// const u=10;
+// const v=20;
+
+// console.log(u == v);
+
+// logical operator----
+
+// and operator in javascript------
+
+
+
+
+
+
+
+
+
