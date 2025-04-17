824123ac37f312feec4273cd2a811d132135ee8c
sunilkuarcse <216sunildd@gmail.com>
2025-04-17 16:05:19 +0000

    strings done

---
+++ 01_basics/04_string.js
@@ -0,0 +1,68 @@
+// let name = "sunil";
+// let age = 21;
+
+// let bio = (`sunil name is : ${name} , and age is : ${age}`)
+// console.log(bio)
+
+// let name = new String("sunildabriya");
+// console.log(typeof name);
+
+// string methods are commonly used in javascripts-----------
+
+// 1.-----.length this method measure the number of charcters in string 
+// let name = "sunildariya";
+// console.log(name.length);
+
+// 2.------concat--this methods are used two string add means combine two string
+// let str1  = "tony";
+// let str2 = "stark"
+// console.log(str1.concat (str2));
+
+// let str1 = "sakti";
+// let str2 = "man";
+// console.log(str1.concat(str2));
+
+// 3.indexof and charAt() is same -----this method used to find the indexof of a specific charectrs----
+// let str1 = "tonystarck";
+// console.log(str1.indexOf("y"));
+
+// let str2 = "sunildabriya";
+// console.log(str2.indexOf("d"));
+
+// 4.-----slice this methods used to extract a portion of a string------
+// let str = "sunildabriya";
+// console.log(str.slice(6)); extract means string ke 6 number wale piche ke portion ko hta diya gya h
+
+// let str = "tonystarck";
+// console.log(str.slice(4));
+
+// 5.----replace---this method used to replace a specific charectrs or subtrings in a string;
+// let str = "hello world!";
+// console.log(str.replace("world!" , "universe!"));
+
+// let str = "tony saktiman";
+// console.log(str.replace("saktiman" , "starck"));
+
+
+// 6.----toUppercase and toLowercass-----------------------
+// let str = "tonystarck"
+// let str1 = "saktiman";
+
+// console.log(str.toUpperCase());
+// console.log(str1.toLowerCase());
+
+// 7.trim--------is used to space end forward and backward;
+// const str  = "    tony    "
+// console.log(str)
+// console.log(str.trim());
+
+// console.log(+true)
+// console.log(+false)
+
+
+
+
+
+
+
+
